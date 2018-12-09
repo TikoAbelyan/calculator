0c2a1856a61195fcbedf8431c82efb7fed6539f6
TikoAbelyan <tikoabelyan1995@gmail.com>
2018-12-09 15:33:51 +0400

    initial commit

---
+++ src/components/ClearButton.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import "./ClearButton.css";
+
+export const ClearButton = props =>(
+    <div className="clear-btn" onClick={props.handleClear}>
+        {props.children}
+    </div>
+)
---
+++ src/components/Input.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import "./Input.css";
+
+export const Input = props =>(
+    <div className="input">{props.input}</div>
+);
